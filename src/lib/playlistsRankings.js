b17d376a37fce0d26e2ca48fab40c29dee146701
Alessandro Talamona <info@talale.it>
2025-09-17 12:04:38 +0200

    Create playlistsRankings.js

---
+++ src/lib/playlistsRankings.js
@@ -0,0 +1,59 @@
+export const rankings = [];
+
+export const headers = [
+  "Keyword",
+  "Market",
+  "Position",
+  "Playlist Spotify ID",
+  "Playlist Name",
+  "Owner Spotify ID",
+];
+
+export function init() {
+  rankings.length = 0;
+}
+
+export function appendContent(playlists, keyword, market) {
+  const newRankings = playlists
+    .map((playlist, position) =>
+      playlist
+        ? {
+            keyword: keyword,
+            market: market,
+            position: position + 1,
+            playlistSpotifyId: playlist.id,
+            playlistName: playlist.name,
+            ownerSpotifyId: playlist.owner?.id || "",
+          }
+        : null
+    )
+    .filter((playlist) => playlist);
+
+  rankings.push(...newRankings);
+}
+
+export function downloadCSV(name = "playlists_rankings") {
+  const csvContent = [
+    headers.join(","),
+    ...rankings.map((row) =>
+      [
+        row.keyword,
+        row.market,
+        row.position,
+        row.playlistSpotifyId,
+        row.playlistName,
+        row.ownerSpotifyId,
+      ].join(",")
+    ),
+  ].join("\n");
+  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
+
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = name + ".csv";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+}
